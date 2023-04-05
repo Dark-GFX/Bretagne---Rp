fc1749d81961abed13b27d2ac8abb7fbc713dbf2
Tom Weus <Tomweus25@gmail.com>
2023-04-05 22:19:42 +0200

    First commit

---
+++ script.js
@@ -0,0 +1,13 @@
+const text = document.getElementById("text");
+const textContent = "🌱 | Bretagne-RP";
+let currentIndex = 0;
+
+function typeText() {
+  text.textContent += textContent[currentIndex];
+  currentIndex++;
+  if (currentIndex >= textContent.length) {
+    clearInterval(timer);
+  }
+}
+
+let timer = setInterval(typeText, 150);
